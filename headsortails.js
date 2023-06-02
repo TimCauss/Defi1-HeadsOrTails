37f264a74fa58a9ee660781ef0a2f73acb1771c3
TimCauss <t.caussignac@gmail.com>
2023-06-02 10:51:22 +0200

    Ajout de la fonction et déplacement de la fonction dans le listener

--- headsortails.js
+++ headsortails.js
@@ -2,7 +2,11 @@ const button = document.getElementsByClassName("button");
 const result = document.getElementsByClassName("p-result");
 
 button[0].addEventListener("click", () => {
+  pileOuFace();
+});
+
+function pileOuFace() {
   let resultText = ["Heads", "Tails"];
   let random = Math.floor(Math.random() * resultText.length);
   result[0].innerHTML += resultText[random] + "<br>";
-});
+}
