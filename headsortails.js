7e1317b845cc24b3bdb003f928b0c253a2aedc01
TimCauss <t.caussignac@gmail.com>
2023-06-05 11:13:13 +0200

    Adding stats

--- headsortails.js
+++ headsortails.js
@@ -1,5 +1,10 @@
 const button = document.getElementById("button");
 const result = document.getElementById("p_result");
+const stats = document.getElementById("stats");
+const pourc = document.getElementById("pourcentages");
+
+let y = 0;
+let x = 0;
 
 button.addEventListener("click", () => {
   pileOuFace();
@@ -8,6 +13,15 @@ button.addEventListener("click", () => {
 function pileOuFace() {
   let resultText = ["Heads", "Tails"];
   let random = Math.floor(Math.random() * resultText.length);
-  console.log(resultText[random]);
-  result.innerHTML += resultText[random] + "<br>";
+  random == 0 ? x++ : y++;
+  result.innerHTML += resultText[random] + " ";
+  stat(x, y);
+}
+
+function stat(headsTotal, tailsTotal) {
+  stats.innerHTML = "Heads: " + headsTotal + " / " + "Tails:  " + tailsTotal;
+  let pourcHeads = ((headsTotal / (headsTotal + tailsTotal)) * 100).toFixed(2);
+  let pourcTails = ((tailsTotal / (headsTotal + tailsTotal)) * 100).toFixed(2);
+  pourc.innerHTML =
+    "Heads: " + pourcHeads + "% / " + "Tails: " + pourcTails + "%";
 }
